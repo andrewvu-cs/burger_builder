fb9f85d65eb2ced26b804ee0f829e1a462e277c3
avu916 <andrewvu.cs@gmail.com>
2019-05-10 13:01:54 -0700

    able to safely remove burger contents and displays the count on build controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,16 @@ import classes from './BuildControl.module.css'
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>-</button>
-        <button className={classes.More} onClick={props.added}>+</button>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>-
+        </button>
+        <p className={classes.label}>{props.count}</p> 
+        <button 
+            className={classes.More} 
+            onClick={props.added}>+
+        </button>
     </div>
 );
 
