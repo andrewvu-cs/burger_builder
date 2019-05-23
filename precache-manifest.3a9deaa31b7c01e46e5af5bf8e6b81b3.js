0154bde409889e78338eb8890c768f63ba8f45e7
avu916 <andrewvu.cs@gmail.com>
2019-05-23 13:49:38 -0700

    Updates

---
+++ precache-manifest.3a9deaa31b7c01e46e5af5bf8e6b81b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f08f512390c4e6dd883d2b33b02112",
+    "url": "/burger_builder/index.html"
+  },
+  {
+    "revision": "c7b0ec1a72b558f2eaf1",
+    "url": "/burger_builder/static/css/main.f3d7b1f6.chunk.css"
+  },
+  {
+    "revision": "9588b7818aaafa06aa61",
+    "url": "/burger_builder/static/js/2.ca2e0295.chunk.js"
+  },
+  {
+    "revision": "c7b0ec1a72b558f2eaf1",
+    "url": "/burger_builder/static/js/main.d2c8c821.chunk.js"
+  },
+  {
+    "revision": "fd3cde94dfbe7ffa1088",
+    "url": "/burger_builder/static/js/runtime~main.bef5c481.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
