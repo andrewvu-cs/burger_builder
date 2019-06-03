c49d3a70074b437e0bdbfca63d3c5a3d56ce8a0f
avu916 <andrewvu.cs@gmail.com>
2019-06-03 14:14:54 -0700

    able to send POST request to firebase with dummy data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -82,7 +83,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            //ideally would want to recalculate the price on the server 
+            //b/c someone might be able to manipulate the data before it is sent
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Andrew Vu',
+                address: {
+                    street: 'test 1213',
+                    zipCode: '99999',
+                    country: 'USA'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'expedited'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render(){
