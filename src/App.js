cf2d4c1ab120356d671845491244d83a0e27aed2
avu916 <andrewvu.cs@gmail.com>
2019-06-06 19:04:25 -0700

    added checkout container, set up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 //Wanted to keep App the 'root' just in case of file restructuring 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder></BurgerBuilder>
+        <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+        </Switch>
       </Layout>
     </div>
   );
