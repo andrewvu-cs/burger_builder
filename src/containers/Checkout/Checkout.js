b83f6f0ef3de6a42c6815308c4fd62862a21b66b
avu916 <andrewvu.cs@gmail.com>
2019-06-19 17:05:57 -0700

    improved UX for purchasing and combined reducers

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -18,25 +18,29 @@ class Checkout extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary 
+        let summary = <Redirect to="/"/>
+        if (this.props.ings){
+            summary = (
+                <div>
+                    <CheckoutSummary 
                     ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinue={this.checkoutContinueHandler}/>
-                <Route 
-                    // Link adjusted
-                    path={this.props.match.path + '/contact-data' } 
-                    component={ContactData} />
-            </div>
-        );
+                    <Route 
+                        // Link adjusted
+                        path={this.props.match.path + '/contact-data' } 
+                        component={ContactData} />
+                </div>
+            );
+        }
+        return summary;
     }
 
 }
 
 const mapStateToProps = state => {
     return{
-        ings: state.ingredients
+        ings: state.burgerBuilder.ingredients
     }
 }
 
