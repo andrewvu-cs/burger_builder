e5f658e9fca3ede5cb831e3280497279ce7b8a0b
avu916 <andrewvu.cs@gmail.com>
2019-06-24 19:29:20 -0700

    now forwards unathenticated users to auth page when trying to purchase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,7 +40,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState({purchasing: true});
+        if (this.props.isAuthenticated ){
+            this.setState({purchasing: true});
+        } else{
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -78,6 +82,7 @@ class BurgerBuilder extends Component {
                         count={this.state.ingredients}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         />
                 </Aux>
                 )
@@ -105,7 +110,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     }
 }
 
