daad71c07eef670262dc2a4dbffc82586e09b47a
avu916 <andrewvu.cs@gmail.com>
2019-06-24 18:16:47 -0700

    able to access protected resources(Orders) via auth token

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -8,42 +8,40 @@ import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-
-    componentDidMount(){
-        this.props.onFetchOrders();
+    componentDidMount () {
+        this.props.onFetchOrders(this.props.token);
     }
-
 
     render () {
         let orders = <Spinner />;
-        if (!this.props.loading) {
-            orders =  this.props.orders.map(order => (
-                <Order 
+        if ( !this.props.loading ) {
+            orders = this.props.orders.map( order => (
+                <Order
                     key={order.id}
                     ingredients={order.ingredients}
-                    price={order.price}
-                />
-            ))
+                    price={order.price} />
+            ) )
         }
-
         return (
             <div>
-              {orders}
+                {orders}
             </div>
         );
     }
 }
+
 const mapStateToProps = state => {
     return {
         orders: state.orders.orders,
-        loading: state.orders.loading
+        loading: state.orders.loading,
+        token: state.auth.token
     };
 };
 
 const mapDispatchToProps = dispatch => {
-    return{
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+    return {
+        onFetchOrders: (token) => dispatch( actions.fetchOrders(token) )
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps) (withErrorHandler(Orders, axios));
+export default connect( mapStateToProps, mapDispatchToProps )( withErrorHandler( Orders, axios ) );
